1c864dfd7eb2fafea18b59bc5189b3c64a7edf31
Ankura <Master-Anku.com>
2024-05-16 23:19:02 +0630

    data Attributes

---
+++ assets/css/script.js
@@ -0,0 +1,20 @@
+var container = document.querySelector(".container");
+
+
+container.addEventListener("click", function(even){
+    var element = even.target;
+
+    if (element.matches(".box")){
+        var state = element.getAttribute("data-state");
+
+        if (state == "hidden") {
+            element.textContent = element.dataset.number;
+            element.dataset.state = "visible";
+        }else {
+            element.textContent = "";
+            element.setAttribute("data-state", "hidden")
+        }
+    }
+
+});
+alert("Hello, ဒီနေ့မှာတော့ Data-Attributes ကို လေ့လာကြည့်လိုက်ရအောင်")
